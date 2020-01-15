76f9bc10bc8810681c71bbbb50ba9f6777631b12
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-15 16:30:16 -0500

    Updates

---
+++ precache-manifest.4c9e14d2ff787bc75e366627901ba505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5667f2aa64a154ed60ee3027edcd0566",
+    "url": "/proyecto-tasa-cambio/index.html"
+  },
+  {
+    "revision": "b01422ec78661da55499",
+    "url": "/proyecto-tasa-cambio/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9e8f8c8337ce0f8c85f5",
+    "url": "/proyecto-tasa-cambio/static/css/main.ca57514d.chunk.css"
+  },
+  {
+    "revision": "b01422ec78661da55499",
+    "url": "/proyecto-tasa-cambio/static/js/2.1d395e8b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/proyecto-tasa-cambio/static/js/2.1d395e8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e8f8c8337ce0f8c85f5",
+    "url": "/proyecto-tasa-cambio/static/js/main.890d1a51.chunk.js"
+  },
+  {
+    "revision": "d74d5a5c4d92e6fb11f7",
+    "url": "/proyecto-tasa-cambio/static/js/runtime-main.024ed845.js"
+  }
+]);
